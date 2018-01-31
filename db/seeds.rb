863a91fbbb27b1c36d4d312a2b382c06ae021559
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-30 19:53:45 -0500

    fix controller issue, add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
 Product.create(name: 'bag1',
   image: 'http://res.cloudinary.com/dooqsrgbq/image/upload/v1517357718/ysl1_gmn0cf.jpg',
   style: '487219DV7261000-BLACK',
@@ -19,3 +21,30 @@ Made in Italy.',
   retail_price: 1990.00,
   price: 1150.00,
   quantity: 4)
+
+  Product.create(name: 'bag2',
+    image: 'http://res.cloudinary.com/dooqsrgbq/image/upload/v1517357718/ysl2_xceu4c.jpg',
+    style: '487219DV7261000-BURG',
+    color: 'Burgundy',
+    description: 'Loulou Monogram Medium Quilted Leather BackpackSaint Laurent "Monogram Loulou" quilted calf leather backpack.Looped top handle.Looped top handle.Adjustable buckle back straps.
+Flap with hidden magnetic closure.
+Open top with threaded drawstring.11.8"H x 11.8"W x 5.5"D.
+Made in Italy. ',
+    retail_price: 1990.00,
+    price: 1150.00,
+    quantity: 1)
+
+    Product.create(name: 'bag3',
+      image: 'http://res.cloudinary.com/dooqsrgbq/image/upload/v1517357718/ysl3_bgmsqk.jpg',
+      style: '487212BRM041242-BLACK',
+      color: 'Black',
+      description: 'Saint Laurent "College" chevron-quilted leather shoulder bag.
+Removable flat top handle, 2.8" drop.
+Detachable shoulder strap, 14.4" drop.
+Flap top with logo medallion.
+Interior, cotton/leather lining.
+7.9"H x 12.6"W x 3.4"D.
+Made in Italy.',
+      retail_price: 2590.00,
+      price: 1476.00,
+      quantity: 4)
