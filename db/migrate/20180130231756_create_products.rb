7983f8d4fe8e409ce3ee0b8938e195d2db4d11f3
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-30 19:10:27 -0500

    create product model and migrations, create route for it, create view page

--- db/migrate/20180130231756_create_products.rb
+++ db/migrate/20180130231756_create_products.rb
@@ -2,6 +2,16 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
 
+      t.string :name, null: false
+      t.string :image, null: false
+      t.string :style, null: false
+      t.string :color, null: false
+      t.text :description, null: false
+      t.float :retail_price, null: false
+      t.float :price, null: false
+      t.integer :quantity, null: false
+
+
       t.timestamps
     end
   end
