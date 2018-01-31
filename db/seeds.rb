bb55a7babdbc9e01ef3ee9e64f27ec3bd406fbce
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-30 19:25:39 -0500

    first seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'bag1',
+  image: 'http://res.cloudinary.com/dooqsrgbq/image/upload/v1517357718/ysl1_gmn0cf.jpg',
+  style: '487219DV7261000-BLACK',
+  color: 'Black',
+  description: 'Loulou Monogram Medium Quilted Leather BackpackSaint Laurent "Monogram Loulou" quilted calf leather backpack.
+Looped top handle.
+Adjustable buckle back straps.
+Flap with hidden magnetic closure.
+Open top with threaded drawstring.
+11.8"H x 11.8"W x 5.5"D.
+Made in Italy.',
+  retail_price: 1990.00,
+  price: 1150.00,
+  quantity: 4)
