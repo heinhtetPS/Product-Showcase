51fd0340ad05a21bbcf99b36244d0ad2c61490a8
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-31 17:49:24 -0500

    add filtering functionality using query strings. add bootstrap for easy dom elements. bootstrap dropdown menu has bug when path changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    if params[:brand]
+      @products = Product.where(:brand => params[:brand])
+    else
+      @products = Product.all
+    end
   end
 end
